a3b58f8775f6c920634897cb3930eed22cce9c17
burak-senel <buurak.senel@gmail.com>
2024-05-20 15:37:29 +0300

    dog barking task done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,31 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock < 0 || clock > 23) {
+            return false;
+        }
+       else if (isBarking && (clock < 8 || clock >= 20)) {
+            return true;
+        }
+        return false;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+       return true;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        return true;
+
     }
 
     public static double area(double width, double height) {
-       
+        return 0.5;
+
     }
 
     public static double area(double radius) {
-       
+        return 0.4;
+
     }
 }
