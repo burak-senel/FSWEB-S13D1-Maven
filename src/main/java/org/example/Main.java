2673a5204916b9ab3a03df4fb762680beaee2c1c
burak-senel <buurak.senel@gmail.com>
2024-05-20 16:17:04 +0300

    hasteen task done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,7 +16,10 @@ public class Main {
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       return true;
+       if((firstAge >=13 && firstAge <=19)||(secondAge >=13 && secondAge <=19)||(thirdAge >=13 && thirdAge <=19)){
+           return true;
+        }
+       return false;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
